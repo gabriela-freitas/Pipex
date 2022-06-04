7cf9cd786f62adc0be5276b4e0911d05483e9970
Gabriela Freitas de albuquerque <gafreita@c1r14s13.42lisboa.com>
2022-06-04 16:58:50 +0100

    after close() the program runs but the pipe looks empty

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: gafreita <gafreita@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 21:45:13 by gafreita          #+#    #+#             */
-/*   Updated: 2022/06/04 16:49:20 by gafreita         ###   ########.fr       */
+/*   Updated: 2022/06/04 16:56:53 by gafreita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,9 @@ int	main(int argc, char **argv, char **envp)
 			if (fd < 0)
 				perror("cannot open file\n");
 			dup2(fd, 0);
+			close(pipe_fd[0]);
 			dup2(pipe_fd[1], 1);
+			// close(pipe_fd[1]);
 			execve(infos()->cmd1[0], infos()->cmd1, envp);
 			printf("something wrong happen\n");
 		}
@@ -44,14 +46,15 @@ int	main(int argc, char **argv, char **envp)
 		{
 			wait(&pid);
 			ft_printf("I've waited\n");
+			close(pipe_fd[1]);
 			dup2(pipe_fd[0], 0);
+			// close(pipe_fd[0]);
 			fd1 = open("outfile", O_WRONLY | O_ASYNC);
 			if (fd1 < 0)
 				perror("cannot open file\n");
 			if (dup2(fd1, 1) < 0)
 				perror("cannot dup\n");
-			ft_printf("heyy\nheyy");
-			// execve(infos()->cmd2[0], infos()->cmd2, envp);
+			execve(infos()->cmd2[0], infos()->cmd2, envp);
 		}
 	}
 	else
